e2ac6b67d3b90c3737e2f51f41f48e2ab9cd35d5
0xFranckx0 <franck.rupin@gmail.com>
2017-08-18 20:53:58 -0400

    Add pkg directory

--- proxy.go
+++ proxy.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/0xFranckx0/go-proxy-search/pkg/rest"
 	"os"
 )
 
 func main() {
 	fmt.Println(os.Getenv("MSG"))
 	fmt.Println("Hello World!")
-
+	rest.Test()
 }
